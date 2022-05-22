73bfc6099c819203c1c5cce5a9730e8dfebc631b
XingZhuang Xu <CarltonXu@users.noreply.github.com>
2022-05-22 05:34:04 +0000

    Finsh math calculator

--- main.py
+++ main.py
@@ -1,13 +1,35 @@
 import sys
 
-def arithmetic_arranger(countlist):
+def arithmetic_arranger(countlist, flag=False):
+    one = ""
+    two = ""
+    three = ""
+    four = ""
     checkListType(countlist)
     checkGiveParamNum(countlist)
     checkParamType(countlist)
     checkOperator(countlist)
     checkParamNumLength(countlist)
+    formatParam(countlist)
+    mathresult = formatParam(countlist)
+    for ma in mathresult:
+        if mathresult.index(ma) == 0:
+            one += ma.split("\n")[0]
+            two += ma.split("\n")[1] 
+            three += ma.split("\n")[2]
+            four += ma.split("\n")[3]
+        else:
+            one += " " * 4 + ma.split("\n")[0]
+            two += " " * 4 + ma.split("\n")[1] 
+            three += " " * 4 + ma.split("\n")[2]
+            four += " " * 4 + ma.split("\n")[3]
+            
+    print(one)
+    print(two)
+    print(three)
+    if flag:
+        print(four)
     
-
 def checkListType(param):
     if not type(param):
         print("Give parameter not list type")
@@ -36,5 +58,33 @@ def checkParamNumLength(param):
             print("Error: Numbers cannot be more than four digits.")
             sys.exit()
 
+def formatParam(param):
+    flist = []
+    for p in param:
+        firstnum = p.split()[0]
+        mathSymbols = p.split()[1]
+        secondnum = p.split()[2]
+        if len(firstnum) > len(secondnum):
+            lengthnum = len(firstnum)
+        else:
+            lengthnum = len(secondnum)
+        lengthnum += 2
+        if len(firstnum) < lengthnum:
+            fone = " " * (lengthnum - len(firstnum)) + firstnum
+        else:
+            fone = firstnum
+        if len(secondnum) < lengthnum:
+            ftwo = mathSymbols + " " * (lengthnum - len(secondnum) -1) + secondnum
+        else:
+            ftwo = mathSymbols + " " + secondnum
+        if mathSymbols == "+":
+            mathresultnum = int(firstnum) + int(secondnum)
+        else:
+            mathresultnum = int(firstnum) - int(secondnum)
+        fthree = fone + "\n" + ftwo + "\n" + ((lengthnum) * "-") + "\n" + (" " * (lengthnum - len(str(mathresultnum))) + str(mathresultnum))
+        flist.append(fthree)
+    return flist
+            
+
 if __name__ == "__main__":
-    arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])
+    arithmetic_arranger(["32 + 8", "1 - 3801", "9999 + 9999", "523 - 49"], True)
